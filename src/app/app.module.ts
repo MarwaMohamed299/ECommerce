adb428c79246e98c07e7819c14998a255a929543
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2023-10-17 19:15:46 +0300

    Hoome+NavBar+Register+Footer+Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './Components/NavBar/nav-bar/nav-bar.component';
+import { HomeComponent } from './Components/Home/home/home.component';
+import { LogInComponent } from './Components/log-in/log-in.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { FooterComponent } from './Components/Footer/footer/Footer/footer.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    LogInComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
