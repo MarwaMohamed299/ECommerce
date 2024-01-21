d766494ff7d4abaef490f29465990698c4ebe621
Jamal <Jamal_Ali@outlook.com>
2024-01-21 08:36:07 +0200

    Thank you Marwa

--- src/app/Components/user-module/register/register.component.ts
+++ src/app/Components/user-module/register/register.component.ts
@@ -1,24 +1,32 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-  standalone:true,
+  standalone: false,
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  imports:[ReactiveFormsModule,CommonModule]
 })
-
 export class RegisterComponent {
+  userRegisterForm: FormGroup = new FormGroup({
+    Name: new FormControl(
+      '',
+      Validators.maxLength(30) && Validators.minLength(3) && Validators.required
+    ),
+    Email: new FormControl('', [Validators.email, Validators.required]),
+    Password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(30),
+      Validators.pattern('^[a-zA-Z0-9]+$'),
+    ]),
+  });
 
-userRegisterForm : FormGroup = new FormGroup({
-  Name:new FormControl('',Validators.maxLength(30) && Validators.minLength(3) && Validators.required),
-  Email: new FormControl('', [ Validators.email, Validators.required]),
-  Password: new FormControl('' , [Validators.required, Validators.minLength(2),Validators.maxLength(30), Validators.pattern("^[a-zA-Z0-9]+$")])
-})
-
-onSubmit(){
-  console.warn(this.userRegisterForm.value)
-}
+  onSubmit() {
+    console.warn(this.userRegisterForm.value);
+  }
 }
--- src/app/Components/user-module/user-module.module.ts
+++ src/app/Components/user-module/user-module.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegisterComponent } from './register/register.component';
-import { RouterModule ,Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-
-
-const routes :Routes = [
+const routes: Routes = [
   { path: 'LogIn', component: LogInComponent },
-  { path: 'Register', component: RegisterComponent}
-]
+  { path: 'Register', component: RegisterComponent },
+];
 @NgModule({
-  declarations: [
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(routes)
-  ]
+  declarations: [RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
 })
-export class UserModuleModule { }
+export class UserModuleModule {}
